899e9a8d608d98c558cc009cada27970d80fd3d5
unknown <clamy6@yahoo.fr>
2021-12-13 14:42:46 +0100

    ex1/ex5

---
+++ jour01.js
@@ -0,0 +1,30 @@
+//exercice 01//
+
+console.log("hello world!");
+
+//exercice 02//
+
+var test;
+test = "My name is Caroline";
+console.log(test);
+
+//exercice 03//
+
+var name;
+name = "Caroline";
+console.log("Nice to meet you " + (name));
+console.log(`Nice to meet you ${name}`);
+
+//exercice 04//
+var testLength;
+testLength = "I m very long";
+console.log(test.length);
+
+//exercice 05//
+
+var food;
+food = "croissant is meh";
+food = food.replace ("meh", "so good")
+console.log(food);
+
+
