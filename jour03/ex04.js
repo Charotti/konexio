914044fa8965f48b769736bd82a6610514137d34
unknown <clamy6@yahoo.fr>
2021-12-15 15:00:05 +0100

    jour03/ex04

---
+++ jour03/ex04.js
@@ -0,0 +1,13 @@
+let numbers = ["4", "10","8", "12", "6"];
+numbers.reverse();
+console.log(numbers);
+
+//croissant
+
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+
+//décroissant
+
+/*numbers.sort((a, b) => b-a);
+console.log(numbers);*/
