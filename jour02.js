31bc51b40a9854442abc2cac22a58c695b7452d8
unknown <clamy6@yahoo.fr>
2021-12-15 14:26:10 +0100

    jour03/ex01

--- jour02.js
+++ jour02.js
@@ -1,5 +1,5 @@
 //Exercice 01//
-
+console.log();
 let integer = 102;
 let float = 13.9;
 console.log(integer, float);
@@ -12,8 +12,8 @@ console.log(stringfield);
 
 //Exercice 03//
 
-let num = 1.5;
-let rounded = Math.round(num);
+const num = 1.5;
+const rounded = Math.round(num);
 console.log(rounded);
 
 //Exercice 04//
@@ -36,14 +36,23 @@ console.log( test %= bis);*/
 
 //Exercice 05//
 
-let test = 143;
-let bis =  219;
-console.log(test > bis, test < bis, test === bis, test !== bis);
+const test = 143;
+const bis =  219;
+console.log(test > bis, test < bis,  test >= bis, test <=bis,  test === bis, test !== bis);
 
 //Exercice 06//
 
 let limit = 50;
 let score = 64;
+const hasWon = score >= limit;
+
+if (!hasWon){
+    console.log( "Oh nooo...")
+}else 
+{
+    console.log("Ok good !")
+};
+
 
 if (score >= limit){
     console.log("Ok good !")
@@ -52,9 +61,11 @@ if (score >= limit){
 console.log( "Oh nooo...")
 };
 
+
+
 //Exercice 07//
 
-let password = "az";
+let password = "azerty"; //az  console.log password is not secure//
 console.log(password.length);
 
 if (password.length > 5 ){
@@ -68,7 +79,7 @@ if (password.length > 5 ){
 if (score >= limit && password.length > 5 ){
     console.log( "Everything is good")
 }
-    if (score >= limit || password.length > 5 ){
+    else if (score >= limit || password.length > 5 ){
        console.log("Something is good"); 
     }
        else{
@@ -90,7 +101,7 @@ if(random === 6){
 };
 
 //Bonus II //
-let month = "march";
+let month = "january";
 
 
 switch (month){
@@ -130,6 +141,39 @@ switch (month){
         console.log("winter");
         break; 
 };
+
+//Bonus III //
+let roundedNumber = 3.6;
+if( roundedNumber-(Math.floor(roundedNumber)) >= 0.5){
+    console.log(Math.ceil(roundedNumber));
+}else{
+    console.log(Math.floor(roundedNumber));
+};
+
+let stringNumber = roundedNumber.toString();
+console.log(stringNumber);
+
+let stringNumberIndex = stringNumber.indexOf(".");
+console.log(stringNumberIndex);
+
+let newNumber = stringNumber.substring(stringNumberIndex + 1);
+console.log(newNumber);
+
+if (newNumber >= 5){
+
+ console.log(Math.ceil(newNumber))
+}else
+{
+    console.log(Math.round(newNumber));
+}
+
+
+
+
+
+
+
+
 
 
 
