5c33b66a95b45702c85e0ac5f491111450d279fa
unknown <clamy6@yahoo.fr>
2021-12-15 14:46:47 +0100

    jour03/ex03

---
+++ jour03/ex03.js
@@ -0,0 +1,9 @@
+let objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+console.log(objects);
+objects.pop();
+console.log(objects);
+objects.push("laptop");
+console.log(objects);
+objects.shift();
+console.log(objects);
