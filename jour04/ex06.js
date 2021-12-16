8dbbe1d638be7ca768c8c49a3020c9c0d50e80bb
unknown <clamy6@yahoo.fr>
2021-12-16 16:32:52 +0100

    jour04/ex06

---
+++ jour04/ex06.js
@@ -0,0 +1,13 @@
+function format (num){
+    let hrs= Math.floor(num/3600);
+
+    let hRest = num - ( hrs * 3600);
+
+    let min = Math.floor(hRest/60);
+
+    let sec = hRest - (min*60);
+
+    console.log (`${hrs} : ${min} : ${sec}`)
+
+};
+format(3700);
