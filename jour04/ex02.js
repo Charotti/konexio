0cf19f92d5b93e2c45b2c7c0f5fd9b941c42c90e
unknown <clamy6@yahoo.fr>
2021-12-16 14:26:06 +0100

    jour4/ex02

---
+++ jour04/ex02.js
@@ -0,0 +1,34 @@
+/*let cat ={
+    name: "Garfield",
+    age: 3,
+    isCute: true
+}
+
+let cat2 ={
+    name: "kitty",
+    age: 5,
+    isCute: false
+}*/
+
+const cats = [
+	cat = {
+		name: "Garfield",
+		age: 3,
+        isCute: true,
+	},
+	 cat2 = {
+		username: "Kitty",
+		age: 5,
+        isCute: false,
+	},
+];
+
+//Valeur de age de cat
+
+console.log(cat.age);
+console.log(cat["age"]);
+
+//valeur de isCute de cat2
+
+console.log(cat2.isCute);
+console.log(cat2["isCute"]);
