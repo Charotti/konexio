14224d8bcae688eede1aee979a7b133089dc7a1c
unknown <clamy6@yahoo.fr>
2021-12-16 15:13:40 +0100

    jour04/ex04

---
+++ jour04/ex04.js
@@ -0,0 +1,13 @@
+function compare ( num1, num2){
+    if ( num1 > num2 ){
+        console.log("num1 is bigger");
+    }
+    if ( num2 > num1 ){
+        console.log("num2 is bigger");
+    }
+    if (num1 === num2){
+        console.log("both are the same");
+    }
+};
+
+compare (32, 47);
