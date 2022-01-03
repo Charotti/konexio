8dd67cd127e6e8f366538ee520630ee6f00548f6
unknown <clamy6@yahoo.fr>
2022-01-03 13:47:20 +0100

    ex2

--- forloops.js
+++ forloops.js
@@ -1,9 +1,19 @@
 //SOMME DES CARRES//
 
-let result = 0;
+/*let result = 0;
 for( let i = 5; i <=10; i++ ){
   result = result + i * i;
 }
-console.log(result);
+console.log(result);*/
 
 //COMPTONS//
+
+let result = 0;
+for (let i = 100; i <= 1000; i++){
+  let modulo= i % 7 ;
+  if (modulo ===0)
+ result = result +1;
+ 
+}
+
+console.log(result);
