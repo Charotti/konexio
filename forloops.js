f9975200b5a6bddd634e07b6e6a4e8955bc10241
unknown <clamy6@yahoo.fr>
2022-01-03 14:36:46 +0100

    ex03

--- forloops.js
+++ forloops.js
@@ -9,11 +9,29 @@ console.log(result);*/
 //COMPTONS//
 
 let result = 0;
-for (let i = 100; i <= 1000; i++){
-  let modulo= i % 7 ;
-  if (modulo ===0)
- result = result +1;
- 
+for (let i = 100; i <= 1000; i++) {
+  let modulo = i % 7;
+  if (modulo === 0) {
+    result = result + 1;
+  }
+
 }
 
 console.log(result);
+
+//chanceux//
+
+
+let result2 = 0;
+
+for (let i = 1; i <= 20; i++) {
+  let launch = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
+  console.log(i, launch);
+  if (launch >= 5) {
+    result2 += launch;
+  }
+}
+console.log(result2);
+
+
+
