82d3411f7faaff37207ad3f463d7c378f6a278a6
unknown <clamy6@yahoo.fr>
2022-01-03 15:39:53 +0100

    ex01-loop

---
+++ whileLoop.js
@@ -0,0 +1,19 @@
+/*var finishLine = 100;
+var athletePosition = 0;
+
+while (athletePosition < finishLine) {
+    ahtletePosition++;
+}*/
+
+
+let number = 50;
+while (number <= 200) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }
+    number++;
+   
+}
+
+
+
