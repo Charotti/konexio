8f9151a5837e91ae7c3f5a171a6399d63d532d1b
unknown <clamy6@yahoo.fr>
2022-01-04 16:19:47 +0100

    ex02-hardLoop

--- hardLoop.js
+++ hardLoop.js
@@ -1,30 +1,68 @@
-//SUM//
+//Ex 01 SUM//
 
-let numbers =[];
+let numbers = [];
 let numRandom = 0;
-let sum= 0;
+let sum = 0;
 
 
 
-for( i = 0; i <= 50; i++) {
+for (i = 0; i <= 50; i++) {
 
-numRandom = Math.floor(Math.random() * (100-0)+1);
-  // console.log(numRandom);
+    numRandom = Math.floor(Math.random() * (100 - 0) + 1);
+    // console.log(numRandom);
 
-  if (numbers.indexOf(numRandom) === -1) {
-    numbers.push(numRandom);
-} else {
-    i--;
-} 
+    if (numbers.indexOf(numRandom) === -1) {
+        numbers.push(numRandom);
+    } else {
+        i--;
+    }
 }
 
 console.table(numbers);
 
 
-for( i= 0; i <= numbers.length-1; i++) {
-  sum+= i;
+for (i = 0; i <= numbers.length - 1; i++) {
+    sum += i;
 
 }
 console.log(sum);
+
+//EX 02 MAX//
+
+
+let numbers = [];
+let numRandom = 0;
+
+
+for (i = 0; i <= 50; i++) {
+    numRandom = Math.floor(Math.random() * (200 - 50 + 1) + 50);
+    console.log(numRandom);
+    if (numbers.indexOf(numRandom) === -1) {
+        numbers.push(numRandom);
+    } else {
+        i--;
+    }
+}
+
+console.table(numbers);
+
+var numMax = numbers[0];
+
+for (var i = 1; i < numbers.length; i++) {
+    var item = numbers[i];
+    console.log(numbers[i])
+
+    if (item > numMax) {
+        numMax = item;
+    }
+}
+
+console.log(numMax);
+
+    //EX 03 UNIQUE//
+    
+
+
+
 
 
