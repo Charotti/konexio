eb8f6859ed471287182a213832b42fe57aa5ce49
unknown <clamy6@yahoo.fr>
2022-01-04 12:06:53 +0100

    ex3

--- forloops.js
+++ forloops.js
@@ -1,23 +1,24 @@
 //SOMME DES CARRES//
 
-/*let result = 0;
+let result = 0;
 for( let i = 5; i <=10; i++ ){
-  result = result + i * i;
+  result +=  i * i;
+  
 }
-console.log(result);*/
+console.log(result);
 
 //COMPTONS//
 
-let result = 0;
+let count = 0;
 for (let i = 100; i <= 1000; i++) {
-  let modulo = i % 7;
-  if (modulo === 0) {
-    result = result + 1;
+  //let modulo = i % 7;//
+  if (i % 7 === 0) {
+    count++;
   }
 
 }
 
-console.log(result);
+console.log(count);
 
 //chanceux//
 
--- whileLoop.js
+++ whileLoop.js
@@ -1,11 +1,4 @@
-/*var finishLine = 100;
-var athletePosition = 0;
-
-while (athletePosition < finishLine) {
-    ahtletePosition++;
-}*/
-
-
+/*//EXERCICE 01 COMPTONS//
 let number = 50;
 while (number <= 200) {
     if (number % 2 === 0) {
@@ -14,6 +7,43 @@ while (number <= 200) {
     number++;
    
 }
+
+//EXERCICE 02 TRY AGAIN//
+
+let dice = 0;
+let count = 0;
+
+while (dice !== 6) {
+    dice = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
+    count++;
+}
+console.log(count);*/
+
+
+//EXERCICE 03//
+
+
+let ussainBolt = 0;
+let tysonGay = 0;
+let winner = "";
+let number1 = Math.floor(Math.random() * ((10 - 1) + 1) + 1);
+let number2 = Math.floor(Math.random() * ((10 - 1) + 1) + 1);
+
+
+while (ussainBolt <= 100 || tysonGay <= 100) {
+    ussainBolt += number1;
+    tysonGay += number2;
+}
+if (ussainBolt >= tysonGay) {
+    winner = "ussainBolt";
+} else {
+    winner = "tysonGay";
+}
+
+console.log(winner);
+
+
+
 
 
 
