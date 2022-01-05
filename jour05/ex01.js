e1004734615a896c6b4ce6dd7d4395ce44d45a48
unknown <clamy6@yahoo.fr>
2022-01-05 15:21:42 +0100

    ex01-jour05

---
+++ jour05/ex01.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+function calculate(num1, operator, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if (operator === "+") {
+        return num1 + num2;
+    } else if (operator === "-") {
+        return num1 - num2;
+    } else if (operator === "/") {
+        return num1 / num2;
+    } else if (operator === "*") {
+        return num1 * num2;
+    }
+
+}
+console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+
+
+
